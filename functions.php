8bb9291cda0d29902d1500c1e048e9fff92ed65d
Caio Brandao <caiobrandz@gmail.com>
2021-08-18 19:22:08 -0300

    add bootstrap

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ add_action('after_setup_theme','my_theme_setup');
 
 function css_files() {
     
-    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap/bootstrap.min.css');
+    wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css');
     
     if ( is_front_page() == true ) {
         // só vai carregar se estiver na front-page
@@ -35,7 +35,7 @@ function script_files(){
 
     wp_enqueue_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js', array('jquery'));
 
-    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap/bootstrap.min.js', array('popper'));
+    wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js', array('popper'));
 }
 add_action('wp_enqueue_scripts', 'script_files');
 
