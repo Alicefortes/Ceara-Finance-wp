1a60d137c90004f324112f6506cd422b7493407e
Alice Fortes <alicefortsp@gmail.com>
2021-09-01 15:27:04 -0300

    Section-CF Educação (estruturação e parte do css)

--- page-projetos.php
+++ page-projetos.php
@@ -16,6 +16,34 @@
             </div>
         </div>
     </div>
+
+    <div class="educacao ">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-10 section-title">
+                    <h3>CF Educação</h3>
+                    <p>Nosso projeto com foco social. A área de educação é responsável por transmitir o conteúdo para a sociedade, com o objetivo de permear o interesse pelo mercado e desenvolver uma cultura local. Principais formas de atuação:</p>
+                </div>
+                <div class="col-md-5 educacao-item">
+                    <h4>Artigos</h4>
+                    <p>Produção e compartilhamento de artigos com temas de mercado.</p>
+                </div>
+                <div class="col-md-5 educacao-item">
+                    <h4>Promoção de eventos</h4>
+                    <p>Promoção de eventos dentro dos espaços universitários com palestras e convidados.</p>
+                </div>
+                <div class="col-md-5 educacao-item">
+                    <h4>CF Escolas</h4>
+                    <p>Projeto com objetivo de produzir cursos introdutórios de finanças e investimentos para alunos do ensino fundamental 2 e médio, de escolas públicas e privadas.</p>
+                </div>
+                <div class="col-md-5 educacao-item">
+                    <h4>Treinamentos e desenvolvimento interno</h4>
+                    <p>Responsáveis pela busca incessante de conteúdos e a identificação de skills necessárias para os membros.
+</p>
+                </div>
+            </div>
+        </div>
+    </div>
     <p>oi</p>
 <main>
 <?php get_footer(); ?>
