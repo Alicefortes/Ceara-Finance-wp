84f05357a070984498a8b140b46d9f9f9169bd56
Alice Fortes <alicefortsp@gmail.com>
2021-09-02 21:56:55 -0300

    sections de projetos

--- page-projetos.php
+++ page-projetos.php
@@ -22,26 +22,94 @@
             <div class="row justify-content-center">
                 <div class="col-md-10 section-title">
                     <h3>CF Educação</h3>
-                    <p>Nosso projeto com foco social. A área de educação é responsável por transmitir o conteúdo para a sociedade, com o objetivo de permear o interesse pelo mercado e desenvolver uma cultura local. Principais formas de atuação:</p>
+                    <p class="section-text" >Nosso projeto com foco social. A área de educação é responsável por transmitir o conteúdo para a sociedade, com o objetivo de permear o interesse pelo mercado e desenvolver uma cultura local. Principais formas de atuação:</p>
                 </div>
                 <div class="col-md-5 educacao-item">
                     <h4>Artigos</h4>
-                    <p>Produção e compartilhamento de artigos com temas de mercado.</p>
+                    <p class="section-text">Produção e compartilhamento de artigos com temas de mercado.</p>
                 </div>
                 <div class="col-md-5 educacao-item">
                     <h4>Promoção de eventos</h4>
-                    <p>Promoção de eventos dentro dos espaços universitários com palestras e convidados.</p>
+                    <p class="section-text">Promoção de eventos dentro dos espaços universitários com palestras e convidados.</p>
                 </div>
                 <div class="col-md-5 educacao-item">
                     <h4>CF Escolas</h4>
-                    <p>Projeto com objetivo de produzir cursos introdutórios de finanças e investimentos para alunos do ensino fundamental 2 e médio, de escolas públicas e privadas.</p>
+                    <p class="section-text">Projeto com objetivo de produzir cursos introdutórios de finanças e investimentos para alunos do ensino fundamental 2 e médio, de escolas públicas e privadas.</p>
                 </div>
                 <div class="col-md-5 educacao-item">
                     <h4>Treinamentos e desenvolvimento interno</h4>
-                    <p>Responsáveis pela busca incessante de conteúdos e a identificação de skills necessárias para os membros.
-</p>
+                    <p class="section-text">Responsáveis pela busca incessante de conteúdos e a identificação de skills necessárias para os membros.</p>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="asset-management" >
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-10 section-title">
+                    <h3>CF Asset Management</h3>
+                    <p  class="section-text">Nosso braço de gestão de patrimônio tem como foco o desenvolvimento dos membros a partir da simulação de fundos de investimento.</p>
+                </div>
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-title">
+                            <h5>Iratus Fia</h5>
+                            <p class="section-text">Fundo simulado de ações</p>
+                        </div>
+                        <div class="card-text">
+                            <p class="section-text texto">Nosso fundo simulado de ações. Através de períodos de análise definida e diversas discussões internas, a equipe do fundo monta o portfólio do Iratus, a partir daí, é feita a manutenção das posições através de outras muitas rodadas de discussão.</p>
+                            <p class="section-text item">Modelagem financeira para Valuation</p>
+                            <p class="section-text item">Gestão de portfólio</p>
+                            <p class="section-text item">Análise de tendências e formação de teses</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-title">
+                            <h5>Iracema Fip</h5>
+                            <p class="section-text">Fundo simulado de participações</p>
+                        </div>
+                        <div class="card-text">
+                            <p class="section-text texto">Nosso fundo simulado de participações. Através de documentos de parceiros, simula-se a relação de um fundo de private equity com o intermediário da venda, incluindo análise de teaser e infomemo e produção de checklists. </p>
+                            <p class="section-text item">Modelagem financeira para Valuation</p>
+                            <p class="section-text item">Análise de empresas privadas</p>
+                            <p class="section-text item">Produção de apresentações</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="equity-reseach">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-10 section-title">
+                    <h3>CF Equity Reseach</h3>
+                    <p  class="section-text">A área de Research da liga tem como objetivo a formação de membros com qualidades competitivas para participação nos challenges.</p>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm">
+                    <div class="equity-reseach-item">
+                        <h4>1</h4>
+                        <p>Construção de uma tese de investimentos. </p>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <div class="equity-reseach-item">
+                        <h4>2</h4>
+                        <p>Produção de apresentações de nível altíssimo de qualidade.</p>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <div class="equity-reseach-item">
+                        <h4>3</h4>
+                        <p>Modelagem financeira para valuation.</p>
+                    </div>
+                </div>
+            </div>
+           
         </div>
     </div>
     <p>oi</p>
