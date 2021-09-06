6ecf567ad96dd7d9af3d5515104d6e605b17c73a
Caio Brandao <caiobrandz@gmail.com>
2021-09-06 03:29:19 -0300

    condicional page do footer

--- footer.php
+++ footer.php
@@ -4,33 +4,52 @@
     <div class="row justify-content-center rodape ">
         <div class="col rodape-item">
             <h4>Páginas</h4>
-           <a href="#"><p>Home</p></a>
-           <a href="#"><p>Projetos</p></a>
-           <a href="#"><p>Equipe</p></a>
-           <a href="#"><p>Blog</p></a>
+            <a href="#"><p>Home</p></a>
+            <a href="#"><p>Projetos</p></a>
+            <a href="#"><p>Equipe</p></a>
+            <a href="#"><p>Blog</p></a>
         </div>
         <div class="col rodape-item">
-             <h4>Seções</h4>
-             <a href="#"><p></p></a>
-           <a href="#"><p>CF Educação</p></a>
-           <a href="#"><p>CF Asset Management</p></a>
-           <a href="#"><p>CF Equity Rsearch</p></a>
+            <h4>Seções</h4>
+            <a href="#"><p></p></a>
+            <?php if ( is_front_page() == true ){
+                echo '
+                    
+                ';
+            }?>
+            <?php if ( is_page('projetos') == true ){
+                echo '
+                    <a href="#"><p>CF Educação</p></a>
+                    <a href="#"><p>CF Asset Management</p></a>
+                    <a href="#"><p>CF Equity Rsearch</p></a>
+                ';
+            }?>
+            <?php if ( is_page('equipe') == true ){
+                echo '
+                    
+                ';
+            }?>
+            <?php if ( is_category() == true ){
+                echo '
+                    
+                ';
+            }?>
         </div>
         <div class="col rodape-item">
-             <h4>Sobre</h4>
-             <p>Rua Humberto Monte, Fortaleza, Ceará 6000, Br</p>
-             <p>+55 (85) 99671-0009</p>
-             <p>cearafinance@gmail.com</p>
-             <div class="social">
-                 <a href="#"><p>i </p></a>
-                 <a href="#"><p>i </p></a>
-                 <a href="#"><p>i </p></a>
-                 <a href="#"><p>i </p></a>
-                 <a href="#"><p>i </p></a>
-             </div>
+            <h4>Sobre</h4>
+            <p>Rua Humberto Monte, Fortaleza, Ceará 6000, Br</p>
+            <p>+55 (85) 99671-0009</p>
+            <p>cearafinance@gmail.com</p>
+            <div class="social">
+                <a href="#"><p>i </p></a>
+                <a href="#"><p>i </p></a>
+                <a href="#"><p>i </p></a>
+                <a href="#"><p>i </p></a>
+                <a href="#"><p>i </p></a>
+            </div>
         </div>
-        <div class="col-md-10 text-center ">
-            <p class="direito" >201X - 2021 CEARÁ FINANCE. Todos os direitos reservados</p>
+        <div class="col-xl-12 text-center ">
+            <p class="direito" >201X - 2021 CEARÁ FINANCE. Todos os direitos reservados.</p>
         </div>
     </div>
 </div>
