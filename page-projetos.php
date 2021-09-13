8710b55cae3166e9d46b066cf6aa85bf89f7feba
Alice Fortes <alicefortsp@gmail.com>
2021-09-13 13:42:49 -0300

    ajustes page-projetos

--- page-projetos.php
+++ page-projetos.php
@@ -25,18 +25,22 @@
                     <p>Nosso projeto com foco social. A área de educação é responsável por transmitir o conteúdo para a sociedade, com o objetivo de permear o interesse pelo mercado e desenvolver uma cultura local. Principais formas de atuação:</p>
                 </div>
                 <div class="col-xl-6 educacao-item">
+                <h3><span class="material-icons-outlined">article</span></h3>
                     <h4>Artigos</h4>
                     <p>Produção e compartilhamento de artigos com temas de mercado.</p>
                 </div>
                 <div class="col-xl-6 educacao-item">
+                    <h3><span class="material-icons-outlined">event </span></h3>
                     <h4>Promoção de eventos</h4>
                     <p>Promoção de eventos dentro dos espaços universitários com palestras e convidados.</p>
                 </div>
                 <div class="col-xl-6 educacao-item">
+                    <h3><span class="material-icons-outlined">school</span></h3>
                     <h4>CF Escolas</h4>
                     <p>Projeto com objetivo de produzir cursos introdutórios de finanças e investimentos para alunos do ensino fundamental 2 e médio, de escolas públicas e privadas.</p>
                 </div>
                 <div class="col-xl-6 educacao-item">
+                    <h3><span class="material-icons-outlined">work_outline</span></h3>
                     <h4>Treinamentos e desenvolvimento interno</h4>
                     <p>Responsáveis pela busca incessante de conteúdos e a identificação de skills necessárias para os membros.</p>
                 </div>
@@ -53,14 +57,27 @@
                 <div class="cardsflex">
                     <div class="card">
                         <div class="card-title">
+
                             <h5>Iratus Fia</h5>
                             <h6>Fundo simulado de ações</h6>
                         </div>
                         <div class="card-text">
+                            <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/aspas.svg" alt="logo" width="15px">
                             <p class="section-text texto">Nosso fundo simulado de ações. Através de períodos de análise definida e diversas discussões internas, a equipe do fundo monta o portfólio do Iratus, a partir daí, é feita a manutenção das posições através de outras muitas rodadas de discussão.</p>
-                            <h6 class="section-text item">Modelagem financeira para Valuation</h6>
-                            <h6 class="section-text item">Gestão de portfólio</h6>
-                            <h6 class="section-text item">Análise de tendências e formação de teses</h6>
+                            
+                            <div class="list-card-item">
+                                <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/hashtag.svg" alt="logo" >
+                                <h6 class="section-text item">Modelagem financeira para Valuation</h6>
+                            </div>
+                            <div class="list-card-item">
+                                <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/hashtag.svg" alt="logo">
+                                <h6 class="section-text item">Gestão de portfólio</h6>
+                            </div>
+                           <div class="list-card-item">
+                                <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/hashtag.svg" alt="logo">
+                                <h6 class="section-text item">Análise de tendências e formação de teses</h6>
+                           </div>
+                           
                         </div>
                     </div>
                     <div class="card">
@@ -69,10 +86,21 @@
                             <h6>Fundo simulado de participações</h6>
                         </div>
                         <div class="card-text">
+                            <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/aspas.svg" alt="logo" width="15px">
                             <p class="section-text texto">Nosso fundo simulado de participações. Através de documentos de parceiros, simula-se a relação de um fundo de private equity com o intermediário da venda, incluindo análise de teaser e infomemo e produção de checklists. </p>
-                            <h6 class="section-text item">Modelagem financeira para Valuation</h6>
-                            <h6 class="section-text item">Análise de empresas privadas</h6>
-                            <h6 class="section-text item">Produção de apresentações</h6>
+                            <div class="list-card-item">
+                                <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/hashtag.svg" alt="logo" >
+                                <h6 class="section-text item">Modelagem financeira para Valuation</h6>
+                            </div>
+                            <div class="list-card-item">
+                                <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/hashtag.svg" alt="logo" >
+                                <h6 class="section-text item">Análise de empresas privadas</h6>
+                            </div>
+                            <div class="list-card-item">
+                                <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/hashtag.svg" alt="logo" >
+                                <h6 class="section-text item">Produção de apresentações</h6>
+                            </div>
+                           
                         </div>
                     </div>
                 </div>
@@ -90,18 +118,21 @@
             <div class="row justify-content-center">
                 <div class="col-4">
                     <div class="equity-research-item">
+                        <h3><span class="material-icons-outlined">savings</span></h3>
                         <h4>1</h4>
                         <p>Construção de uma tese de investimentos. </p>
                     </div>
                 </div>
                 <div class="col-4">
                     <div class="equity-research-item">
+                        <h3><span class="material-icons-outlined">poll</span></h3>
                         <h4>2</h4>
                         <p>Produção de apresentações de nível altíssimo de qualidade.</p>
                     </div>
                 </div>
                 <div class="col-4">
                     <div class="equity-research-item">
+                        <h3><span class="material-icons-outlined">trending_up</span></h3>
                         <h4>3</h4>
                         <p>Modelagem financeira para valuation.</p>
                     </div>
